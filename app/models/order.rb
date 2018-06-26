2286e566def73a2cc7272274a34cf57534aa709b
Jordan Huang <jordanhuang99@gmail.com>
2018-06-26 03:41:49 +0000

    finished testing for model

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,16 @@ class Order < ActiveRecord::Base
   monetize :total_cents, numericality: true
 
   validates :stripe_charge_id, presence: true
-  # after_create
+  after_create :reduct_by_quantity
+
+  def reduct_by_quantity
+    self.line_items.each do |item|
+      if item.product.quantity == 0
+        return 0
+      else 
+        item.product.update_attribute('quantity', item.product.quantity - item.quantity)
+      end
+    end
+  end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -7,29 +7,35 @@ RSpec.describe Order, type: :model do
     before :each do
       # Setup at least two products with different quantities, names, etc
       @category = Category.find_or_create_by! name: 'Apparel'
-      @product1 = @category.Product.create!(
+      @product1 = @category.products.create!(
         name: "yeezy",
         price: 2000,
         quantity: 10
       )
-      @product2 = @category.Product.create!(
+      @product2 = @category.products.create!(
         name: "supreme",
         price: 350,
-        quantity: 20
+        quantity: 0
       )
       # Setup at least one product that will NOT be in the order
     end
     # pending test 1
-    xit 'deducts quantity from products based on their line item quantities' do
+    it 'deducts quantity from products based on their line item quantities' do
       # TODO: Implement based on hints below
       # 1. initialize order with necessary fields (see orders_controllers, schema and model definition for what is required)
       @order = Order.new(
         email: 'jordanhuang99@gmail.com',
         total_cents: 200000,
-
+        stripe_charge_id: 123
       )
+
       # 2. build line items on @order
-      # ...
+      @order.line_items.new(
+        product: @product1,
+        quantity: 1,
+        item_price: @product1.price,
+        total_price: 1 * @product1.price
+      )
       # 3. save! the order - ie raise an exception if it fails (not expected)
       @order.save!
       # 4. reload products to have their updated quantities
@@ -37,10 +43,28 @@ RSpec.describe Order, type: :model do
       @product2.reload
       # 5. use RSpec expect syntax to assert their new quantity values
       # ...
+      expect(@product1.quantity).to eq(9)
     end
     # pending test 2
-    xit 'does not deduct quantity from products that are not in the order' do
-      # TODO: Implement based on hints in previous test
+    it 'does not deduct quantity from products that are not in the order' do
+      @order = Order.new(
+        email: 'jordanhuang99@gmail.com',
+        total_cents: 200000,
+        stripe_charge_id: 123
+      )
+
+      @order.line_items.new(
+        product: @product2,
+        quantity: 1,
+        item_price: @product2.price,
+        total_price: 1 * @product2.price
+      )
+
+      @order.save!
+
+      @product2.reload
+
+      expect(@product2.quantity).to eq(0)
     end
 
   end
